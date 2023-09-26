ecc8a6116f32e90e999ee5d3a958df4b522ea96f
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-09-26 17:18:54 +0100

    chore: added new frontend allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
     'http://127.0.0.1:5173',
-    'http://www.fiki-techNotes/',
-    'http://fiki-techNotes/',
+    'https://flexisaf-notes-frontend.vercel.app/'
 ];
 
 module.exports = allowedOrigins;
