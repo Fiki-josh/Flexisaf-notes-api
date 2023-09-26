5c0145ddc016067efa28b54c3fd4ed748258c155
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-09-26 18:02:54 +0100

    added origin changed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://127.0.0.1:5173',
-    'https://flexisaf-notes-frontend.vercel.app/'
+    'https://flexisaf-notes-frontend.vercel.app'
 ];
 
 module.exports = allowedOrigins;
